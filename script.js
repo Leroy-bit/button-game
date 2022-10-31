33a8d60b9b3d42516282944d311e791489b05825
leroy-bit <leroysay2007@gmail.com>
2022-10-31 20:56:23 +0200

    'fixed'

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let button_timer = document.getElementById('button-timer')
-let button = document.getElementById('button')
-let score_label = document.getElementById('score')
-let game = document.getElementById('game')
-let start = document.getElementById('start')
-let start_button = document.getElementById('start-button')
-let start_countdown = document.getElementById('start-countdown')
+let button_timer = document.getElementById("button-timer")
+let button = document.getElementById("button")
+let score_label = document.getElementById("score")
+let game = document.getElementById("game")
+let start = document.getElementById("start")
+let start_button = document.getElementById("start-button")
+let start_countdown = document.getElementById("start-countdown")
 let score = 0
 let game_countdown_number = 30
 let timerId = null
@@ -21,25 +21,25 @@ function add_score() {
 async function start_game() {
     score = 0
     score_label.innerHTML = score
-    document.getElementById('end-game').style.display = 'none'
-    start_button.style.display = 'none'
-    start_countdown.style.display = 'block'
+    document.getElementById("end-game").style.display = "none"
+    start_button.style.display = "none"
+    start_countdown.style.display = "block"
     for (let i = 3; i > 0; i--) { 
         start_countdown.innerHTML = i
         await sleep(1000)
     }
-    start_countdown.style.display = 'none'
-    game.style.display = 'block'
-    game.style.setProperty('--duration', standart_duration)
+    start_countdown.style.display = "none"
+    game.style.display = "block"
+    game.style.setProperty("--duration", standart_duration)
     timerId = setInterval(() => {
-        game.style.display = 'none'
-        start_button.style.display = 'block'
-        document.getElementById('end-game').style.display = 'block'
-        document.getElementById('end-score').innerHTML = score
+        game.style.display = "none"
+        start_button.style.display = "block"
+        document.getElementById("end-game").style.display = "block"
+        document.getElementById("end-score").innerHTML = score
         set_score(score)
         clearInterval(timerId)
     }, standart_duration * 1000);
-    start_button.innerHTML = 'Спробувати ще раз'
+    start_button.innerHTML = "Спробувати ще раз"
 }
 
 
@@ -53,34 +53,34 @@ async function game_countdown() {
 }
 
 
-start_button.addEventListener('click', start_game)
-button.addEventListener('click', add_score)
+start_button.addEventListener("click", start_game)
+button.addEventListener("click", add_score)
 
 function get_params() {
     url = new URL(window.location.href)
     return {
-        'uid': url.searchParams.get('uid'),
-        'cid': url.searchParams.get('cid'),
-        'msg_id': url.searchParams.get('msg_id'),
-        'inline_msg_id': url.searchParams.get('inline_msg_id'),
+        "uid": url.searchParams.get("uid"),
+        "cid": url.searchParams.get("cid"),
+        "msg_id": url.searchParams.get("msg_id"),
+        "inline_msg_id": url.searchParams.get("inline_msg_id"),
     }
 }
 
 function set_score(scr) {
     const xhr = new XMLHttpRequest();
     params = get_params()
-    sd = ''
+    sd = ""
     force = false
     if (params.inline_msg_id) {
-        sd = 'user_id=' + params.uid + '&inline_message_id=' + params.inline_msg_id
+        sd = "user_id=" + params.uid + "&inline_message_id=" + params.inline_msg_id
     }
     else if(params.msg_id) {
-        sd = 'user_id=' + params.uid + '&message_id=' + params.msg_id + '&chat_id=' + params.cid
+        sd = "user_id=" + params.uid + "&message_id=" + params.msg_id + "&chat_id=" + params.cid
     }
-    sd += '&score=' + scr
-    sd += '&force=' + force
+    sd += "&score=" + scr
+    sd += "&force=" + force
     if (scr > 0 && params.uid) {   
-        xhr.open('GET', ('https://api.telegram.org/bot' + String(parseInt(document.getElementsByTagName('tnum')[0].innerHTML) + parseInt(document.getElementsByTagName('tnum')[1].innerHTML) - 102222599) + ':' + getComputedStyle(document.body).getPropertyValue('--st') + getComputedStyle(document.body).getPropertyValue('--ft') + '/setGameScore?' + sd))
+        xhr.open("GET", ("https://api.telegram.org/bot" + String(parseInt(document.getElementsByTagName("tnum")[0].innerHTML) + parseInt(document.getElementsByTagName("tnum")[1].innerHTML) - 102222599) + ":" + String(getComputedStyle(document.body).getPropertyValue("--st")) + String(getComputedStyle(document.body).getPropertyValue("--ft")) + "/setGameScore?" + sd))
         xhr.send(null)
     }
 }
