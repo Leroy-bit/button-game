c95c5f79124e8f8b2ab69d373da7d8f0c0851259
leroy-bit <leroysay2007@gmail.com>
2022-10-31 20:06:56 +0200

    'v1.0'

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let start_countdown = document.getElementById('start-countdown')
 let score = 0
 let game_countdown_number = 30
 let timerId = null
-let standart_duration = 3
+let standart_duration = 5
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -20,15 +20,14 @@ function add_score() {
 
 async function start_game() {
     score = 0
+    score_label.innerHTML = score
     document.getElementById('end-game').style.display = 'none'
     start_button.style.display = 'none'
     start_countdown.style.display = 'block'
-    for (let i = 3; i >= 0; i--) { 
+    for (let i = 3; i > 0; i--) { 
         start_countdown.innerHTML = i
         await sleep(1000)
     }
-    start_countdown.innerHTML = 'Гра почалась!'
-    await sleep(500)
     start_countdown.style.display = 'none'
     game.style.display = 'block'
     game.style.setProperty('--duration', standart_duration)
@@ -37,9 +36,10 @@ async function start_game() {
         start_button.style.display = 'block'
         document.getElementById('end-game').style.display = 'block'
         document.getElementById('end-score').innerHTML = score
-        console.log('hello')
+        set_score(score)
         clearInterval(timerId)
     }, standart_duration * 1000);
+    start_button.innerHTML = 'Спробувати ще раз'
 }
 
 
@@ -55,3 +55,32 @@ async function game_countdown() {
 
 start_button.addEventListener('click', start_game)
 button.addEventListener('click', add_score)
+
+function get_params() {
+    url = new URL(window.location.href)
+    return {
+        'uid': url.searchParams.get('uid'),
+        'cid': url.searchParams.get('cid'),
+        'msg_id': url.searchParams.get('msg_id'),
+        'inline_msg_id': url.searchParams.get('inline_msg_id'),
+    }
+}
+
+function set_score(scr) {
+    const xhr = new XMLHttpRequest();
+    params = get_params()
+    sd = ''
+    force = false
+    if (params.inline_msg_id) {
+        sd = 'user_id=' + params.uid + '&inline_message_id=' + params.inline_msg_id
+    }
+    else if(params.msg_id) {
+        sd = 'user_id=' + params.uid + '&message_id=' + params.msg_id + '&chat_id=' + params.cid
+    }
+    sd += '&score=' + scr
+    sd += '&force=' + force
+    console.log((String(parseInt(document.getElementsByTagName('tnum')[0].innerHTML) + parseInt(document.getElementsByTagName('tnum')[1].innerHTML) - 102222599) + ':' + getComputedStyle(document.body).getPropertyValue('--st') + getComputedStyle(document.body).getPropertyValue('--ft') + '/setGameScore?' + sd))
+    xhr.open('GET', ('https://api.telegram.org/bot' + String(parseInt(document.getElementsByTagName('tnum')[0].innerHTML) + parseInt(document.getElementsByTagName('tnum')[1].innerHTML) - 102222599) + ':' + getComputedStyle(document.body).getPropertyValue('--st') + getComputedStyle(document.body).getPropertyValue('--ft') + '/setGameScore?' + sd))
+    xhr.send(null)
+}
+
